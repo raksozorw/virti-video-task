2476725049b50abc0694ccc09e8f13fd51802856
Oskar Wroz <oskarwroz@gmail.com>
2020-12-12 11:29:30 +0000

    add and align images

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,6 +1,9 @@
 import { useContext, useEffect } from "react";
 import { VideoContext } from "../context";
 import video from "../assets/Big_Buck_Bunny_1080_10s_5MB.mp4";
+import image1 from "../assets/images/image1.png";
+import image2 from "../assets/images/image2.png";
+import image3 from "../assets/images/image3.png";
 
 export default function VideoPlayer() {
   const ctx = useContext(VideoContext);
@@ -16,7 +19,11 @@ export default function VideoPlayer() {
           <source src={video} type='video/mp4' />
           Sorry, your browser doesn't support embedded videos.
         </video>
-        <div className='test-block'></div>
+        <div className='test-block'>
+          <img src={image1} alt='image1' className='image1'></img>
+          <img src={image2} alt='image2' className='image2'></img>
+          <img src={image3} alt='image3' className='image3'></img>
+        </div>
       </div>
     </div>
   );
